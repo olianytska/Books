a10aee15197d5015cc1d5a8469a7f3864710df42
Liza Olianyts'ka <63195522+olianytska@users.noreply.github.com>
2020-06-04 15:00:58 +0300

    Test

---
+++ Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public BookController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            //return View();
+            return Json(new { data= await _db.Book.ToListAsync()});
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.ID == id);
+            if(bookFromDb==null)
+            {
+                return Json(new { success = false, message = "Error while Deliting" });
+
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+
+            return Json(new { success = true, message = "Delete successful" });
+        }
+    }
+}
---
+++ Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookListRazor.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDBContext _db;
+
+        public IndexModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if(book==null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
